85d8a92ea7c98ab445645638edb6c8b3fdd0f66c
mmmeyers <marsi286@gmail.com>
2015-11-17 12:33:22 -0500

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,44 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above 
   # add the second argument, which is a supply, to BOTH the 
   # Christmas AND the New Year's arrays
-  
+  holiday_hash[:winter].each do |holiday, supply_array|
+    supply_array << supply
+  end
 end
  
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  
+  holiday_hash[:spring].each do |holiday, supply_array|
+    supply_array << supply
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  
+
+  new_season = {
+    season.to_sym => {
+    holiday_name.to_sym => supply_array
+    }
+  }
+
+  holiday_hash.merge!(new_season)
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+  holiday_hash[:winter].values.join(" ")
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +66,25 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+
+holiday_hash.each do |season, data|
+  season = season.to_s.capitalize + ":"
+  puts season
+
+  data.each do |attribute, value|
+    attribute = attribute.to_s.split("_").map(&:capitalize).join(" ")
+    value = value.join(", ")
+    puts "  #{attribute}: #{value}"
+  end
 end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  first = holiday_hash[:spring].keys
+  second = holiday_hash[:summer].keys
+  third = second << first
+  third.flatten
 end
-
-
-
-
-
-
-
+end
