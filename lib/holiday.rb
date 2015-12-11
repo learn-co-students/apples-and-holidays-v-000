64c2338550f53440f9ad3707661d38aa2a865c75
codeesh <vineshpersaud@gmail.com>
2015-12-11 10:29:00 -0500

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,34 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above 
   # add the second argument, which is a supply, to BOTH the 
   # Christmas AND the New Year's arrays
-  
+ holiday_hash[:winter].each do |x,y|
+  y << supply
+end
 end
  
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  
+  holiday_hash[:spring][:memorial_day]<< supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
   
 end
 
@@ -53,13 +57,36 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+   holiday_hash.each do |x,y|
+      puts "#{x.capitalize}:"
+
+      y.each do |x,y| 
+            a = ""
+                if x.to_s.include? '_'
+                  a = x.to_s.gsub!('_', ' ')
+                else
+                  a = x.to_s
+                end
+            puts "  #{a.split.map(&:capitalize).join(' ')}: #{y.flatten.join(", ")}"
+          end  
+    end  
 end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+   
 
+  collection =[]
+      holiday_hash.each do |hash, season|
+        season.each do |season,holiday|
+            if holiday.include? "BBQ"
+              collection << season
+            end
+  end   
+  end 
+collection
 end
 
 
