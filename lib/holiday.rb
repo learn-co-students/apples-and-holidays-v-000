a54270be13a634dda38f2d1222233f70eecc7c5b
CodeByLine <yleventhal@ymail.com>
2016-02-21 05:21:08 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,5 @@
 require 'pry'
 
-def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -18,31 +17,42 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-end
+ def second_supply_for_fourth_of_july(holiday_hash)
+ holiday_hash.collect do |season, holiday|
+    holiday.collect do |name, elem|
+      if name == :"fourth_of_july"
+        puts elem[1]
+      end
+    end
+  end
+ end
+
+
+
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above 
-  # add the second argument, which is a supply, to BOTH the 
+  # holiday_hash is identical to the one above
+  # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  
+
 end
- 
+
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  
+
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  
+
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
