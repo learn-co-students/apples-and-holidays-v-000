e0c7ebab0ad22b0ed0405acc60dc29033b9e5e76
Christian Whitesides <whitesidescr@gmail.com>
2016-03-30 15:58:59 -0700

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,34 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july].last
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above 
   # add the second argument, which is a supply, to BOTH the 
   # Christmas AND the New Year's arrays
-  
+  holiday_hash[:winter].each { |holiday, data| data << supply }
 end
  
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  
+  holiday_hash.fetch(season)[holiday_name] = supply_array
+
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +56,30 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+  holiday_hash.each do |season, data_hash|
+    puts season.to_s.capitalize << ":"
+    data_hash.each do |holiday, supplies_array|
+      holiday_name_array = holiday.to_s.split("_").collect do |word|
+        word.capitalize
+      end
+      puts "  " << holiday_name_array.join(" ") << ": " << supplies_array.join(", ")
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  keys_with_bbq = []
+  
+  holiday_hash.each do |season, data_hash|
+    data_hash.each do |holiday, supplies_array|
+      if supplies_array.include?("BBQ")
+        keys_with_bbq << holiday
+      end
+    end
+  end
+  keys_with_bbq
 end
 
 
