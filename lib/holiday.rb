b08392be61d365b5f2d96cd675e4ddafeeb47814
ndaniele <n.daniele@gmail.com>
2016-05-06 15:29:41 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,17 +49,17 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-supply_array = []
+  supply_array = []
 
   holiday_hash.each do |season, details|
     if season == :winter 
-  details.each do |holiday, supplies|
-    supplies.each do |supply|
-  supply_array << supply 
-  end
+      details.each do |holiday, supplies|
+        supplies.each do |supply|
+          supply_array << supply 
+        end
+      end
     end
   end
-    end
   supply_array 
 end
 
@@ -72,32 +72,15 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash.collect do |season, details|
-    season = season.capitalize.to_s
-    puts "#{season}:"
-
-    holiday_str = " "
-    supply_str = " "
-
-    counter = 0
+  holiday_hash.each do |season, details|
+    season = season.capitalize
+      puts "#{season}:"
 
     details.each do |holiday, supplies|
-      holiday = holiday.capitalize.to_s
-      holiday_str << holiday
-      
-      supplies.each do |supply|
-        supply_str = supply_str + supply + ","
-      end
-      spl = supply_str.split
-      if spl.last == ","
-        then spl.pop
-      end
-        supply_str = spl.join
-      end
-      puts " #{holiday_str}:" "#{supply_str}"
-
-      counter += 1
-    end 
+      holiday_text = holiday.to_s.split("_").map(&:capitalize).join(" ")
+        puts "  #{holiday_text}: #{supplies.join(", ")}"
+    end
+  end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -105,20 +88,16 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
     
   holiday_arr = []
-
-  holiday_hash.collect do |season, details|
-  
-  details.each do |holiday, supplies|
-
-  supplies.each do |supply|
-    if supply == "BBQ" 
-       holiday_arr << holiday
-
-end
-end
-end
-end
-holiday_arr
+    holiday_hash.collect do |season, details|
+      details.each do |holiday, supplies|
+        supplies.each do |supply|
+          if supply == "BBQ" 
+          holiday_arr << holiday
+          end
+        end
+      end
+    end
+  holiday_arr
 end
 
 
