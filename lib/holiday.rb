d6032a8280f086fff13843e33495c881b4043cb1
Jesse Novotny <jessenovotny@gmail.com>
2016-06-16 20:30:17 -0700

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,39 +51,16 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each do |holiday, supplies|
       holiday = holiday.to_s
       holiday = holiday.split("_")
-      if holiday.length < 2
-        holiday = holiday.join
-        holiday = holiday.capitalize! 
-      else
+      
         holiday.map {|each_word| each_word = each_word.capitalize!}
         holiday = holiday.join(" ")
-      end
+      
       puts "  #{holiday}: #{supplies.join(", ")}"
     end
   end
 end
 
-
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-
-
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   bbq_days = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
