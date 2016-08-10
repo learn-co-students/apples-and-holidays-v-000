85c897542139408d40966166b4e57ff88dd550bf
Kenlyn Terai <kkterai@gmail.com>
2016-08-10 23:57:52 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -6,7 +6,7 @@ def add_supply_to_winter_holidays(holiday_supplies,items)
   holiday_supplies.each do |season, holiday|
     if season == :winter
         holiday.each do |holiday, supply|
-          holiday[supply] >> items
+          items >> holiday[supply]
         end
     end
   end
