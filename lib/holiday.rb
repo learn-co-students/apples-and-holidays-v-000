4a101eebfd608aa057c7c351d89e6f77878dd20b
John Pfingst <jpfingst@outlook.com>
2016-11-13 05:54:06 +0000

    removing extra comments

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,19 +73,4 @@ def all_holidays_with_bbq(holiday_hash)
     }
   }
   holiday_names
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
 end
