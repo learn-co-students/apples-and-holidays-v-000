c55f1086b8bd6b7d70657ec5765ed3f61e5108cb
charlie lee <ctlee9096@gmail.com>
2017-01-11 02:22:15 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,8 +55,8 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_supplies["#{season}"].push("#{holiday_name}")
-  holiday_supplies["#{season}"]["#{holiday_name}"].push("#{supply_array}")
+  holiday_supplies[#{season}].push(#{holiday_name})
+  holiday_supplies[#{season}][#{holiday_name}].push(#{supply_array})
   binding.pry
   return holiday_supplies
 end
