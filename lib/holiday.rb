e1aaadb899e1021540be0af835c749d9b8d77715
charlie lee <ctlee9096@gmail.com>
2017-01-11 02:15:01 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,18 +48,17 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   #  }
   #supply = gets.chomp
   holiday_supplies[:spring][:memorial_day].push("#{supply}")
+  #binding.pry
   return holiday_supplies[:spring][:memorial_day]
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_supplies[:spring][:memorial_day][:memorial_day_supplies]={}
-  holiday_supplies[:spring][:memorial_day][:memorial_day_supplies]<<"BBQ"
-  holiday_supplies[:spring][:memorial_day][:memorial_day_supplies]<<"Grill"
-  holiday_supplies[:spring][:memorial_day][:memorial_day_supplies]<<"Table Cloth"
-  #binding.pry
-  return holiday_supplies[:spring][:memorial_day][:memorial_day_supplies]
+  holiday_supplies["#{season}"].push("#{holiday_name}")
+  holiday_supplies["#{season}"]["#{holiday_name}"].push("#{supply_array}")
+  binding.pry
+  return holiday_supplies
 end
 
 def all_winter_holiday_supplies(holiday_hash)
