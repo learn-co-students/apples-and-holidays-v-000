baa7966c4117fd0e546495385d8aed6de540a2b2
charlie lee <ctlee9096@gmail.com>
2017-01-11 04:37:46 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,9 +56,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   #binding.pry
-  holiday_name = supply_array
-  holiday_hash[season][holiday_name]
-  binding.pry
+  holiday_hash[season][holiday_name]=supply_array
+  #holiday_name = supply_array
+  #binding.pry
   return holiday_hash
 end
 
