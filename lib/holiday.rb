9a6e28dea098d6c809b5b0f1e970c8c231c30681
Saad Ahmed <dropheaven@live.com>
2017-04-10 09:28:27 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,6 +17,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
+
+    
   # return the second element in the 4th of July array
 end
 
@@ -61,10 +63,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
