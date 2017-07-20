2c94478e9db0d39de2f736e6d43c0d553820786d
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-07-20 19:05:41 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,6 +24,27 @@ def add_supply_to_memorial_day(holiday_hash, supply)
     end
   end
 end
+
+def add_new_holiday_with_supplies(holiday_hash,season,holiday,holiday_supplies)
+  holiday_hash[season][holiday]=holiday_supplies
+end
+
+def all_winter_holiday_supplies(holiday_hash)
+  # return an array of all of the supplies that are used in the winter season
+  new_array = []
+  holiday_hash[:winter].each do |holiday,supplies|
+    new_array << supplies
+    #if season == :winter
+    #  holiday.each do |holiday,supplies|
+    #    p holiday
+    #    p supplies
+    #    holiday_hash[:winter][holiday][supplies]
+    #  end
+  #  end
+  end
+  new_array.flatten
+end
+
 # given that holiday_hash looks like this:
 # {
 #   :winter => {
@@ -40,18 +61,16 @@ end
 #     :memorial_day => ["BBQ"]
 #   }
 # }
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season] << {holiday_name => supply_array}
-  holiday_hash
 
-end
-
-def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
-end
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season,holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do|holiday, supplies|
+      puts "  #{holiday.to_s.split("_").map{|word| word.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
+
+  end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -63,7 +82,13 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  holiday_array =
+  holiday_hash.map do |season,holidays|
+    holidays.map do |holiday, supplies|
+      if supplies.include?("BBQ")
+        new_array = holiday
+      end
+    end
+  end
+  holiday_array.flatten.compact
 end
