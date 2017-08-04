4a7d2e855f4f54a66b27f9d2de705f1ae32ec23c
Wendy Lau <wlau903@gmail.com>
2017-08-04 04:56:26 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,6 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   winter = holiday_hash[:winter]
   winter.values.flatten
-
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -80,5 +79,5 @@ def all_holidays_with_bbq(holiday_hash)
       end
       end
     end
-    return new_array
+    new_array
   end
