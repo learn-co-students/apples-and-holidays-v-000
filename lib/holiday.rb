4dcb7e7416827a017c430309f7d8483586e087f5
Juliana baggio <baggiojuliana@gmail.com>
2017-09-07 03:20:55 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,8 +82,4 @@ def all_holidays_with_bbq(holiday_hash)
       end
   end
   return new_array
-
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
 end
