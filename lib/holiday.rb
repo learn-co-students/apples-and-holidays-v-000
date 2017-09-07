683d2bd571772ea5427ac16b581ce441a792de89
Justin Tooley <jstooley823@gmail.com>
2017-09-07 12:35:19 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,37 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
+
+
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +59,37 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  array = []
+  holiday_hash.each do |season, holiday|
+    holi_array = []
+    puts "#{season.to_s.capitalize!}:"
+    holiday.each do |holiday_two, stuff|
+      array = holiday_two.to_s.split("_")
+      array.each do |word|
+        word.capitalize!
+      end
 
+
+      puts "  #{array.join(" ")}: #{stuff.join(", ")}"
+    end
+    #puts "  #{holi_array.join(" ")}: #{stuff.values}"
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_holiday = []
+  i = 0
+  holiday_hash.each do |seaon, holiday|
+    holiday.each do |holiday_two, stuff|
+      stuff.each do |word|
+        if word == "BBQ"
+          bbq_holiday[i] = holiday_two
+          i+= 1
+        end
+      end
+    end
+  end
+  bbq_holiday
 end
-
-
-
-
-
-
-
