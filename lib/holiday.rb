f462a665a0b3c9db33b446180ce085faa395c2a3
Christopher DiStasio <cadistasio@gmail.com>
2017-10-19 20:58:34 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,14 +19,28 @@ def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 
-def add_supply_to_winter_holidays(holiday_supplies)
-  holiday_supplies.each do |season, holiday|
-    if season == winter
-      holiday << "supply"
-    end
-  end
+# def add_supply_to_winter_holidays(holiday_supplies, supply)
+#   holiday_supplies.each do |season, holiday|
+#     if season == :winter
+#       holiday << "supply"
+#     end
+#   end
+# end
+
+def add_supply_to_winter_holidays(holiday_supplies, supply)
+  holiday_supplies[:winter][:christmas] << supply
+  holiday_supplies[:winter][:new_years] << supply
 end
 
-# def add_supply_to_winter_holidays(holiday_supplies)
-#   holiday_supplies[:winter][:christmas][:new_years] = "supply"
-# end
+def add_supply_to_memorial_day(holiday_supplies, supply)
+  holiday_supplies[:spring][:memorial_day] << supply
+end
+
+def add_new_holiday_with_supplies(holiday_supplies, season, holiday, supplies_for_new_holiday)
+  holiday_supplies[season]
+  holiday_supplies[season][holiday] = supplies_for_new_holiday
+end
+
+def all_winter_holiday_supplies(holiday_supplies)
+  holiday_supplies[:winter].flatten
+end
