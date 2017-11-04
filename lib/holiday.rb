baf21c691846d8b890e6452cecabf44384043673
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-04 02:55:40 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,35 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+ # holiday_hash is identical to the one above
+ # add the second argument, which is a supply, to BOTH the
+ # Christmas AND the New Year's arrays
+ holiday_hash[:winter].each {|holiday, items| items << supply}
 end
+
+
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring].each {|holiday, item| item << supply}
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -54,17 +58,24 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize!}:"
+      holiday.each do |day, item|
+        puts "  #{day.to_s.gsub("_", " ").split.map{|i| i.capitalize!}.join(" ")}: #{item.join(", ")}"
+      end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  f = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |day, array|
+      if array.include?("BBQ")
+        f << day
+    end
+    end
+  end
+  f
 end
-
-
-
-
-
-
-
