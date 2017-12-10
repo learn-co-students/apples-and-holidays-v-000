6bfcc9f4df7ee26d2f1ff5b31c89b28235e0f662
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-10 18:11:48 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,35 +53,58 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   end
 end
 
+# def all_winter_holiday_supplies(holiday_hash)
+#   # return an array of all of the supplies that are used in the winter season
+#   return holiday_hash[:winter]
+#   binding.pry
+# end
+#
+#
+# def all_supplies(holiday_supplies)
+#
+#       # holiday == :fourth_of_july
+#       # we took the holiday key and converted a string
+#       # str_holiday == fourth_of_july
+#       # we took that converted string, and split it to an array with the elements divided by the _ (which removes the _)
+#       # str_holiday == ["fourth", "of", "july"]
+#       # we collected over that array in order to capitalize each word
+#       # str_holiday == ["Fourth", "Of", "July"]
+#       # we joined the elements together to form a string where each element is divded by an empty space
+#       # str_holiday == "Fourth Of July"
+#
+#       # supplies == ["BBQ", "Fireworks"]
+#       # we joined it together converting the array to a string dividing each element by a , and a space
+#       # supplies == "BBQ, Fireworks"
+#       # "  Fourth Of July: BBQ, Fireworks"
+#
+# end
+#
+#
+# def all_holidays_with_bbq(holiday_hash)
+#   # return an array of holiday names (as symbols) where supply lists
+#   # include the string "BBQ"
+#
+# end
+
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-  return holiday_hash[:winter]
-  binding.pry
+  holiday_supplies[:winter].map do |holiday, supplies|
+    supplies
+  end.flatten
 end
 
-
-def all_supplies(holiday_supplies)
-
-      # holiday == :fourth_of_july
-      # we took the holiday key and converted a string
-      # str_holiday == fourth_of_july
-      # we took that converted string, and split it to an array with the elements divided by the _ (which removes the _)
-      # str_holiday == ["fourth", "of", "july"]
-      # we collected over that array in order to capitalize each word
-      # str_holiday == ["Fourth", "Of", "July"]
-      # we joined the elements together to form a string where each element is divded by an empty space
-      # str_holiday == "Fourth Of July"
-
-      # supplies == ["BBQ", "Fireworks"]
-      # we joined it together converting the array to a string dividing each element by a , and a space
-      # supplies == "BBQ, Fireworks"
-      # "  Fourth Of July: BBQ, Fireworks"
-
+def all_supplies_in_holidays(holiday_hash)
+  holiday_supplies.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
+  end
 end
-
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
+    end
+  end.flatten.compact
 end
