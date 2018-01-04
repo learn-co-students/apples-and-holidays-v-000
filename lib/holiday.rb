bbc14e3f942da45955081d4d815a5168e47496be
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-04 19:37:26 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,8 +25,16 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  #my original solution
+=begin
 holiday_hash[:winter][:christmas] << supply
 holiday_hash[:winter][:new_years] << supply
+=end
+
+#since we are apply to EACH winter holiday we can use each enumerable
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
 end
 
 
@@ -40,13 +48,27 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-holiday_hash[season][holiday_name] = supply_array
-holiday_hash
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
+
+  #remember we don't need to worry about what data type. the placeholder will
+  #work for anything that is passed to it. so we don't need to put [:season],
+  #just season will do!!
+
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+
   # return an array of all of the supplies that are used in the winter season
+  #my solution. Let the record show: I like mine better
 holiday_hash[:winter].values.flatten
+
+#their solution
+=begin
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies
+  end.flatten
+=end
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -69,8 +91,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holidays_with_bbq = []
-  holiday_hash.each do |season, data|
-    data.each do |holiday, supplies|
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supplies|
       supplies.select do |supply|
         if supply == "BBQ"
         holidays_with_bbq << holiday
@@ -79,4 +101,13 @@ def all_holidays_with_bbq(holiday_hash)
     end
   end
   holidays_with_bbq
+
+
+  #we could also do the following underneath holiday.each do
+=begin
+  holidays.map do |holiday, supplies|
+    holiday if supplies.include? == ("BBQ")        #this statement returns holiday or nil if true#
+  end
+end.flatten.compact #this combines all arrays and removes 'nil'
+=end
 end
