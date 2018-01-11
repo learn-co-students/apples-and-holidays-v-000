c34696df0fd68d988f39e60e9e04e70d8faa1906
Allyson Wesman <allysonwesman@gmail.com>
2018-01-10 23:02:49 -0800

    updated solution

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,15 +81,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_names = []
 
-  holiday_hash.each do |season, holidays|
-    holidays.each do |holiday, supplies|
-      if supplies.include?("BBQ")
-        holiday_names << holiday
-      end
+  holiday_hash.collect do |season, holidays|
+    holidays.collect do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
     end
-  end
-
-  return holiday_names
+  end.flatten.compact
 end
