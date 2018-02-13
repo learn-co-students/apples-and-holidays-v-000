ee53cfb886ef399d01081812ebe51de6a01b9f94
Robert W <robwash@gmail.com>
2018-02-13 20:23:20 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,30 +1,13 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
-  holiday_hash[:summer][:fourth_of_july][1]
+    holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+  # holiday_hash[:winter].each do |day, items|
+  #  items << supply
+
   holiday_hash.each do |season, holiday|
       holiday.each do |day, items|
         if day == :christmas || day == :new_years
@@ -36,9 +19,9 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-  holiday_hash.each do |season, holiday|
+  # holiday_hash[:spring][:memorial_day] << supply
+
+    holiday_hash.each do |season, holiday|
       holiday.each do |day, items|
         if day == :memorial_day
         items.push(supply)
