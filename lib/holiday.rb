0cbe4ed5421574b70c71199b6e472fd1a86ffaf6
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-13 16:41:30 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,11 +45,10 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holidays|
    puts "#{season.capitalize}:"
    holidays.each do |holiday, supplies|
-   puts "#{holiday.to_s.split('_').collect{ |holiday| holiday.capitalize.join(' '):"+"#{supplies.join(' ,')"
+   puts "#{holiday.to_s.split('_').collect{ |holiday| holiday.capitalize.join(' ')} :#{supplies.join(', ')}"
     end
    end
   end
-
 
 def all_holidays_with_bbq(holiday_hash)
   holidays.find_all do |supply| 
