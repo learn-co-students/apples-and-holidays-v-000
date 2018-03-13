b9c86d48b42ebfb341190d77b495a544e1acefe1
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-13 14:44:23 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   # {
@@ -20,12 +18,13 @@ def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash[:summer][:fourth_of_july][1]
 end
 
+
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter][:christmas] << supply
   holiday_hash[:winter][:new_years] << supply
 end
 
-
+require "pry"
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] << supply
 end
@@ -33,7 +32,6 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
-
 end
 
 def all_winter_holiday_supplies(holiday_hash)
@@ -43,17 +41,19 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
- holiday_hash.each do |seasons, holidays|
-   puts "#{seasons.capitalize}:"
+ holiday_hash.each do |season, holidays, supplies|
+   puts "#{season.capitalize}:"
    holidays.each do |holiday, supplies|
-      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
-    end
-  end
+     puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
+ end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   holidays.find_all do |supply| 
 end
+end
+
 
 
 
