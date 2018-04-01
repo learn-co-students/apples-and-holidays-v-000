07d0486b9f3305decba2c5232b623b82f12b5783
smithwebtek <brad@smithwebtek.com>
2018-03-31 21:31:12 -0400

    Update lab

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,15 +31,16 @@ def all_supplies_in_holidays(holiday_hash)
 		end
 	end
 end
-
+ 
+	
 def all_holidays_with_bbq(holiday_hash)
-  bbq_array = []
-  holiday_hash.each do |season, holidays|
-    holidays.each do |name, values|
-      if holidays[name].include?("BBQ"); 
-        bbq_array << name
-				end
+	bbq_array = []
+	holiday_hash.each do |season, holidays|
+		holidays.each do |name, values|
+			if holidays[name].include?("BBQ"); 
+				bbq_array << name
 			end
 		end
+	end
 	bbq_array 
 end
