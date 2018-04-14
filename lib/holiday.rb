1cee9b89c3efcd4c808dbba7aa2e4a356db70d3e
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-14 00:59:11 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,15 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 
 end
 
@@ -31,28 +34,49 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
+
+  # Prints Seasons
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+      # Prints Holidays
+      holiday.each do |key, value|
+        if key.to_s.include?("_")
+          new_array = key.to_s.split("_")
+
+          new_array.map! do |word|
+            word.capitalize
+          end
+          print "  #{new_array.join(" ")}: "
+        else
+          print "  #{key.to_s.capitalize}: "
+        end
+          # Prints Decorations
+        value.each do |decorations|
+          if value[value.length - 1] != decorations
+            print "#{decorations}, "
+          else
+            print "#{decorations}\n"
+          end
+        end
+      end
+    end
+  end
 
 end
 
@@ -61,10 +85,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
