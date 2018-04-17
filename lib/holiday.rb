a227017c9f4fedab11392307e1a878e9a1fb839f
Anuradha Khambete <anukhambete@gmail.com>
2018-04-17 04:23:45 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,15 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 
 end
 
@@ -31,17 +34,19 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,13 +58,39 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season,data|
+  puts "#{season.capitalize}:"
+  
+  data.each do |holiday,supplies|
+    hol = holiday.to_s
+    holsplit = []
+    holsplit = hol.split('_')
+    arr_cap = []
+    
+        holsplit.each do |word|
+          arr_cap << word.capitalize
+        end
+        holiday_modified = arr_cap.join(" ")
+        
+        puts "  #{holiday_modified}: #{supplies.join(", ")}"
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  ret_array = []
+  holiday_hash.each do |season,data|
+    data.each do |holiday,supply|
+      supply.each do |single_supply|
+        if single_supply == "BBQ"
+          ret_array << holiday
+        end
+      end
+    end
+  end
+  ret_array
 end
 
 
