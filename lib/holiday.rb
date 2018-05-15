0a466dad8642d7ccb0fec67ff7e9a68aec4069ee
Dale Terrell <tragicid@gmail.com>
2018-05-15 16:25:53 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,14 +21,25 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-   holiday_supplies[:winter][:item3] = "Balloons"
+  holiday_hash[:winter].each do |holiday, supplies|
+  # binding.pry
+   supplies<<supply
 end
+end
+
 
 
 # def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:memorial_day].each do |holiday, supplies|
+  # binding.pry
+   supplies<<supply
+end
+
+end
 #end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
