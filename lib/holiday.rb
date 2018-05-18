24610675daa47f0fd02578e6117b2c1667c60c38
Dale Terrell <tragicid@gmail.com>
2018-05-18 18:31:57 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,13 +46,7 @@ end
 
 
 
-def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-end
+
   
 # def holday_supplies fourth_of_july
 #   [:Fireworks]
@@ -81,12 +75,37 @@ def all_winter_holiday_supplies(holiday_hash)
   .flatten
 end
 
+  # return an array of all of the supplies that are used in the winter season
+
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash[:holiday].map do |holiday, supplies|
-    supplies
-    end
-  .flatten
+  # iterate through holiday_hash and print items such that your readout resembles:
+  # Winter:
+  #   Christmas: Lights, Wreath
+  #   New Years: Party Hats
+  # Summer:
 end
+
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holidays|
+    puts season.to_s.capitalize+":"
+    holidays.each do |holiday, supplies|
+      split_words = []
+      holiday.to_s.split("_").each do |word|
+        split_words << word.capitalize
+      end
+      capitalized=split_words.join(" ")
+   # binding.pry
+    puts "  #{capitalized}: #{supplies.join(", ")}"
+    #binding.pry
+  end
+ end
+ 
+ def all_holidays_with_bbq
+  
+end
+
+
 
 
 
