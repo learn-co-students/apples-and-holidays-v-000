b46f04fba15ccdc87388eac0cd698ebdd30d44e8
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-29 19:01:43 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,19 +48,19 @@ end
 
 
 def all_supplies_in_holidays(holiday_hash)
+
    holiday_supplies.each do |season, holiday, supply|
-    season.to_s 
-    holiday.to_s
-    supply.to_s
-    puts ("#{season}:").capitalize! 
-    puts (":#{holiday}:").capitalize! ("#{supply}").capitalize!"
-  end
-  return arr
+    :season.to_s
+   
+    puts "#{season}:".capitalize!
+    holiday.each do |holiday, supplies| 
+      puts  "  #{holiday.to_s.split("_").map {|x| x.capitalize!}.join(" ")}: #{supplies.join(", ")}"
+    end
+   end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+ 
 
 end
 
