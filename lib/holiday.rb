079095270452678f5366f8f88f4e8e720f0af240
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-11 20:50:40 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,18 +35,21 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+holiday_hash[season][holiday_name] = supply_array
+holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].collect do |holiday, supply|
+supply
+end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -57,13 +60,23 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+    holiday_hash.map do |season, holidays|
+      holidays.map do |holiday, supplies|
+        holiday if supplies.include?("BBQ")
+      end
+    end.flatten.compact
+  
 end
 
 
