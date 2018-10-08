60b4437413f7b722ecfb169176b7ad5f6cee444b
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-08 02:22:29 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,9 +22,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
   holiday_hash[:winter][:christmas] << supply
   holiday_hash[:winter][:new_years] << supply
 
@@ -32,21 +29,15 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
   holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-  holiday_hash[:fall] = :columbus_day
-  holiday_hash[:fall][:columbus_day] = supply_array
+  holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
 holiday_hash[:winter].values.flatten
 end
 
@@ -58,16 +49,26 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holiday, supplies|
-    puts "#{season}:
-    #{holiday}: #{supplies}"
+  holiday_hash.each do |season, holiday_details|
+    puts "#{season.capitalize}:"
+    holiday_details.each do |holiday, supplies|
+    "#{holiday.split(":").capitalize}"
+    end
   end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_holidays = []
+  holiday_hash.each do |season, holiday_details_hash|
+    holiday_details_hash.each do |holiday, supplies|
+      if supplies == "BBQ"
+        bbq_holidays << holiday
+      end
+    end
+  end
+  bbq_holidays
 end
 
 
