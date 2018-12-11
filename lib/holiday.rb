7819cadac406904143978fa6434dce9401e395d8
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-12-11 23:20:26 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
+#CHANGES
+
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   # {
@@ -19,40 +21,31 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array
 
-  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:winter].each do |h,s|
-    s << supply
-  end
+
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  holiday_hash[:spring][:memorial_day] << supply
+
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  
-  holiday_hash[season][holiday_name] = supply_array
+
  
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  all_sup = []
- holiday_hash[:winter].each do |hol,sup|
-   all_sup << sup
- end
- all_sup.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -63,26 +56,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
-  holiday_hash.each do |season, holidays|
-    puts "#{season.capitalize}:"
-    holidays.each do |holiday,supplies|
-      puts "  #{holiday.to_s.split("_").map{|w|w.capitalize}.join(" ")}: #{supplies.join(", ")}"
-    end
-  end
+
   
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  arr = []
-  holiday_hash.each do |seas, holidays|
-    holidays.each do |hol, sup|
-        arr << hol if sup.include?("BBQ")
-    end
-  end
-  arr
 
 end
 
