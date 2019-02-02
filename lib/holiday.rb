77176e860f7d86b3f3ca74e2c17aac09760bb79b
Carden Z <ephemere@protonmail.com>
2019-02-02 06:35:16 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,30 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+	holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+  # add  a supply, to BOTH the Christmas AND the New Year's arrays
+	holiday_hash[:winter].each do |key, value|
+    value << supply
+	end
 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+	holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash[season] = {holiday_name => supply_array}
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+	holiday_hash[:winter].values.flatten
 
 end
 
@@ -50,21 +50,21 @@ def all_supplies_in_holidays(holiday_hash)
   # Winter:
   #   Christmas: Lights, Wreath
   #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  holiday_hash.each do |season, holidays|
+		puts "#{season.capitalize}:"
+		holidays.each do |holiday, data|
+      puts "  #{holiday.to_s.split('_').map(&:capitalize).join(' ')}: #{data.join(", ")}"
+		end
+	end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  # return array of holiday names (as symbols) where supply lists include "BBQ"
+  find_bbq(holiday_hash)
 end
 
-
-
-
-
-
-
+def find_bbq(holiday_hash)
+	holiday_hash.map do |season, holidays|
+    holidays.select{|h, s| s.include?("BBQ")}.keys
+	end.flatten
+end
